e421730335fb82029fc706fbacc509ddb837fa08
Zach <dukeman254@gmail.com>
2026-09-15 16:59:32 -0500

    Working on framebuffer

---
+++ starterCode/renderlib/FrameBuffer.cpp
@@ -0,0 +1,19 @@
+#include "FrameBuffer.h"
+#include "png++/png.hpp"
+
+FrameBuffer::FrameBuffer() {};
+
+
+void FrameBuffer::clearToColor(color&) {
+
+}
+
+void setPixelColor (int i, int j, const color& c){
+    for (i = 0; i < fb.length(); i++){
+        
+    }
+}
+
+void exportAsPNG ( std::string filename) {
+
+}
